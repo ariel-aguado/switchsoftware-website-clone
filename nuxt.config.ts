1500c18d2fc941d771dfe761b205031efcbf9d49
Ariel Aguado Albear <ariel.841207@gmail.com>
2022-10-30 18:20:39 -0400

    Add main libraries

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  modules: [
+    '@vueuse/nuxt',
+    '@nuxt/image-edge',
+    '@nuxtjs/tailwindcss',
+    'nuxt-icon',
+    'nuxt-security',
+  ],
 })

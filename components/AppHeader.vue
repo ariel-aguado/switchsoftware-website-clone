508388bdaec389ac8dba595ad0b87f36f09731fb
Ariel Aguado Albear <ariel.841207@gmail.com>
2022-10-31 19:45:50 -0400

    Fixed header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-full">
+  <header class="w-full fixed top-0 z-20">
     <div class="w-full bg-white">
       <div class="h-24 flex justify-between max-w-6xl mx-auto p-6">
         <slot name="logo" />

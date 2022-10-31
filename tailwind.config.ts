61f2e69b07215bd3ba208be60d84feaf55cead8f
Ariel Aguado Albear <ariel.841207@gmail.com>
2022-10-30 20:11:04 -0400

    Add tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+export default {
+  darkMode: 'class',
+  plugins: [
+    // require('@tailwindcss/typography'),
+    // require('@tailwindcss/forms')
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6366F1',
+      },
+    },
+  },
+}

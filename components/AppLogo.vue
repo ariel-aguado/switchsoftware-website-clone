5ff110952ac2b3af0a4fcf8d3fba6f937a0eb2e4
Ariel Aguado Albear <ariel.841207@gmail.com>
2022-10-31 21:47:28 -0400

    Change breakpoint of logo resize

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -1,5 +1,5 @@
 <template>
   <NuxtLink to="/" class="z-10">
-    <NuxtImg src="/logo-switch.svg" alt="Logo" class="w-28 h-28 sm:w-40 sm:h-40 transition-all" />
+    <NuxtImg src="/logo-switch.svg" alt="Logo" class="w-28 h-28 md:w-40 md:h-40 transition-all" />
   </NuxtLink>
 </template>

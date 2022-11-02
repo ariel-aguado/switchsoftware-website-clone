d1ec37fa3ef20e4be9386860af35b1f7d718d481
Ariel Aguado Albear <ariel.841207@gmail.com>
2022-11-02 18:12:34 -0400

    Add awards section

--- components/AppAwards.vue
+++ components/AppAwards.vue
@@ -1,5 +1,119 @@
 <template>
-  <div class="w-full min-h-screen bg-purple-500 flex justify-center items-center">
-    Awards
-  </div>
+  <section class="w-full bg-white">
+    <div
+      class="
+      flex flex-col items-center
+      max-w-2xl sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-6xl
+      mx-auto px-4 pb-2"
+    >
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div class="flex justify-center items-center mt-8 lg:mt-0 order-2 lg:order-1">
+          <NuxtImg
+            class="w-[60%] lg:w-3/4 h-auto transition-all"
+            src="/clutch_img.png"
+            alt="Clutch"
+            width="691"
+            height="741"
+          />
+        </div>
+        <div class="order-1 lg:order-2">
+          <div
+            class="
+            w-full
+            px-4
+            flex flex-col justify-center
+            mt-10
+            z-0"
+          >
+            <h2 class="max-w-sm text-[length:var(--font-size-lg)] text-gray-900 leading-[5rem]">
+              Our Awards
+            </h2>
+            <p class="text-gray-800 mt-3">
+              Clutch is an international review curation platform that collects feedback from businesses about other companies that they’ve worked with. Using a formula that includes online reviews as well as other factors, they rank companies based on location and industry.
+              <br>
+              <br>
+              Switch sees third-party ‘verification’ companies, like Clutch, as key to bringing transparency to the global software outsourcing industry.
+            </p>
+            <div class="flex flex-wrap items-center mt-8">
+              <NuxtLink
+                to="#"
+                class="
+                w-full sm:w-min h-12
+                px-8 mr-0 sm:mr-4 mb-4
+                text-sm text-white font-semibold uppercase
+                rounded-full
+                flex items-center justify-center
+                whitespace-nowrap
+                bg-violet-600
+                transition-colors
+                hover:bg-violet-500
+                active:bg-violet-600
+                focus:outline-none
+                focus:ring-2
+                focus:ring-violet-600
+                focus:ring-offset-2
+                focus:ring-offset-violet-50"
+              >
+                Read More
+              </NuxtLink>
+              <NuxtLink
+                to="#"
+                class="
+                w-full sm:w-min h-12
+                px-8 mb-4
+                text-sm text-white font-semibold uppercase
+                rounded-full
+                flex items-center justify-center
+                whitespace-nowrap
+                bg-violet-600
+                transition-colors
+                hover:bg-violet-500
+                active:bg-violet-600
+                focus:outline-none
+                focus:ring-2
+                focus:ring-violet-600
+                focus:ring-offset-2
+                focus:ring-offset-violet-50"
+              >
+                See more on clutch
+              </NuxtLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Quotes -->
+    <AppCarousel />
+  </section>
 </template>
+
+<style lang="scss">
+.swiper-pagination {
+  position: relative !important;
+  text-align: left !important;
+  margin-top: 2rem !important;
+
+  & .swiper-pagination-bullet {
+    width: .8rem;
+    height: .8rem;
+    cursor: pointer;
+  }
+
+  & .swiper-pagination-bullet-active {
+    background: #ff69b4;
+    transition: box-shadow .2s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+    &:focus {
+      outline: 2px solid transparent;
+      outline-offset: 2px;
+      --tw-ring-color: theme('colors.pink.500');
+      --tw-ring-offset-width: 2px;
+      --tw-ring-offset-color: #f8fafc;
+      --tw-ring-offset-shadow: var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color);
+      --tw-ring-shadow: var(--tw-ring-inset) 0 0 0 calc(2px + var(--tw-ring-offset-width)) var(--tw-ring-color);
+      box-shadow: var(--tw-ring-offset-shadow),var(--tw-ring-shadow),var(--tw-shadow,0 0 #0000) !important;
+    }
+  }
+}
+</style>
---
+++ components/AppCarousel.vue
@@ -0,0 +1,120 @@
+<script lang="ts" setup>
+import { Autoplay, Pagination } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+const modules = ref([Autoplay, Pagination])
+const pagination = ref({
+  clickable: true,
+  renderBullet: (index: string, className: string) => `<span class="${className}"></span>`,
+})
+</script>
+
+<template>
+  <div class="max-w-2xl sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-6xl mx-auto mt-20">
+    <NuxtImg
+      class="w-48 h-auto mb-8"
+      src="/quotes.svg"
+      alt="Quotes"
+      width="292"
+      height="167"
+    />
+    <div class="px-8 sm:px-16">
+      <Swiper
+        :modules="modules"
+        :pagination="pagination"
+        :autoplay="{ delay: 5000 }"
+        :slides-per-view="1"
+        :space-between="50"
+      >
+        <SwiperSlide>
+          <p class="text-gray-800 leading-8">
+            Switch Software Solutions is open and honest. They aren’t afraid to confront or present alternate suggestions and solutions when necessary. They take ownership and treat our projects as their own. Their team is professional and easy to communicate with. We’re very happy with them.
+          </p>
+          <p class="font-merriweather text-2xl text-gray-900 font-semibold mt-14">
+            Igor Polouektov
+          </p>
+          <p class="text-gray-500 mt-1">
+            President of Insurance Services, Allied World Assurance Co.
+          </p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p class="text-gray-800 leading-8">
+            Their skills, their efficiency, and their quick responses to requests stand out. We have a very good relationship with them, they are very proactive, and they always provide great advice.They take very good care of their employees and motivate and train them. This is what makes them stand out from other companies.
+          </p>
+          <p class="font-merriweather text-2xl text-gray-900 font-semibold mt-14">
+            Diego Maye
+          </p>
+          <p class="text-gray-500 mt-1">
+            Site Operations & LAC Cloud Engineering Manager, Verifone
+          </p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p class="text-gray-800 leading-8">
+            I think what really separates them from traditional development companies is how much they seem to care about us. They consistently simplify the process and handle the development without making our lives difficult. We have seen a big bump in our engagement that I think can be credited to the work that Switch did. It has been a great experience working with them so far, and I am looking forward to continuing to do so in the future.
+          </p>
+          <p class="font-merriweather text-2xl text-gray-900 font-semibold mt-14">
+            Max Ibata-Arens
+          </p>
+          <p class="text-gray-500 mt-1">
+            Business Development Manager, Logisyn Advisors
+          </p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p class="text-gray-800 leading-8">
+            They have excellent developers and project managers who do high-quality work. They were very responsive. Also, the code they produced is very high quality and written in a way that makes it easy for us to maintain it long-term. The new Javascript library they have developed has been a huge hit with both existing and new customers.
+          </p>
+          <p class="font-merriweather text-2xl text-gray-900 font-semibold mt-14">
+            Keith Kacin
+          </p>
+          <p>Product Manager, Chargify</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p class="text-gray-800 leading-8">
+            The support of all the company’s staff to our project is impressive. If some details have been missed, they have worked on them to solve them out.
+          </p>
+          <p class="font-merriweather text-2xl text-gray-900 font-semibold mt-14">
+            Guillermo Jacobi
+          </p>
+          <p class="text-gray-500 mt-1">
+            CTO, Zonamerica
+          </p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p class="text-gray-800 leading-8">
+            While building our M&A firm I met a fantastic development company based in Uruguay, that helped me on a few tech projects: Switch Software Solutions. They have incredible knowledge and domain expertise while keeping a reasonable cost. Even more important is their high integrity. I wish we could have known this firm back in the day.
+          </p>
+          <p class="font-merriweather text-2xl text-gray-900 font-semibold mt-14">
+            Cris Arens
+          </p>
+          <p class="text-gray-500 mt-1">
+            Managing Partners at Logisyn Advisors
+          </p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p class="text-gray-800 leading-8">
+            I was really impressed with my Switch team. I did not imagine I would be getting such good, smart ideas along with excellent design work. I want to thank Switch for making it happen for us.
+          </p>
+          <p class="font-merriweather text-2xl text-gray-900 font-semibold mt-14">
+            Michael Pallotto
+          </p>
+          <p class="text-gray-500 mt-1">
+            Co-founder of Skriply
+          </p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p class="text-gray-800 leading-8">
+            Switch took care of the Project Design and Management of a mobile App for our staff and members. I have a Master of Science Degree in Software Engineering, and it was refreshing and exciting to observe modern Software Development in action on this diverse team. The project was a success, on time and on budget. I look forward to expanding our portfolio of smartphone Apps for our businesses with them.
+          </p>
+          <p class="font-merriweather text-2xl text-gray-900 font-semibold mt-14">
+            Arnie Johnson
+          </p>
+          <p class="text-gray-500 mt-1">
+            President & Chair Board of Directors Thrive Workplace
+          </p>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  </div>
+</template>
